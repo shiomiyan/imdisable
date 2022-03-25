cc43a3840fa1dfe569b9544289a630c5a1355917
shiomiyan <rhpav7@protonmail.com>
2022-03-25 20:41:54 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use windows::Win32::Foundation::LPARAM;
+use windows::Win32::Foundation::WPARAM;
+use windows::Win32::UI::Input::Ime::ImmGetDefaultIMEWnd;
+use windows::Win32::UI::Input::Ime::IMC_SETOPENSTATUS;
+use windows::Win32::UI::WindowsAndMessaging::GetForegroundWindow;
+use windows::Win32::UI::WindowsAndMessaging::SendMessageA;
+use windows::Win32::UI::WindowsAndMessaging::WM_IME_CONTROL;
+
+fn main() -> windows::core::Result<()> {
+    unsafe {
+        let fw = GetForegroundWindow();
+        let ime = ImmGetDefaultIMEWnd(fw);
+
+        let res = SendMessageA(
+            ime,
+            WM_IME_CONTROL,
+            WPARAM(IMC_SETOPENSTATUS as usize),
+            LPARAM(0),
+        );
+
+        println!("{:?}", res);
+    }
+    Ok(())
 }
