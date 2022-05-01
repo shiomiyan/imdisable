3391db2de5cebc32a7de945a2e3c6cf9d343526e
shiomiyan <sk736b@protonmail.com>
2022-05-01 17:51:23 +0900

    disable ime with Ctrl-[ kb shortcut

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,27 @@ use windows::Win32::Foundation::LPARAM;
 use windows::Win32::Foundation::WPARAM;
 use windows::Win32::UI::Input::Ime::ImmGetDefaultIMEWnd;
 use windows::Win32::UI::Input::Ime::IMC_SETOPENSTATUS;
+use windows::Win32::UI::Input::KeyboardAndMouse;
 use windows::Win32::UI::WindowsAndMessaging::GetForegroundWindow;
 use windows::Win32::UI::WindowsAndMessaging::SendMessageA;
 use windows::Win32::UI::WindowsAndMessaging::WM_IME_CONTROL;
 
-fn main() -> windows::core::Result<()> {
+fn main() {
+    let mut hk = hotkey::Listener::new();
+    hk.register_hotkey(
+        hotkey::modifiers::CONTROL,
+        KeyboardAndMouse::VK_OEM_4.0 as u32,
+        || {
+            println!("Ctrl-[ pressed.");
+            imdisable().expect("WARN: couldn't disable ime.");
+        },
+    )
+    .unwrap();
+
+    hk.listen();
+}
+
+fn imdisable() -> windows::core::Result<()> {
     unsafe {
         let fw = GetForegroundWindow();
         let ime = ImmGetDefaultIMEWnd(fw);
