3d29a8726fac9de3ef521981d7ab44ccdc5ac0d9
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2023-07-27 20:21:48 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,14 @@
-use windows::Win32::Foundation::LPARAM;
-use windows::Win32::Foundation::WPARAM;
-use windows::Win32::UI::Input::Ime::ImmGetDefaultIMEWnd;
-use windows::Win32::UI::Input::Ime::IMC_SETOPENSTATUS;
-use windows::Win32::UI::Input::KeyboardAndMouse;
-use windows::Win32::UI::WindowsAndMessaging::GetForegroundWindow;
-use windows::Win32::UI::WindowsAndMessaging::SendMessageA;
-use windows::Win32::UI::WindowsAndMessaging::WM_IME_CONTROL;
+use device_query::{DeviceQuery, DeviceState, Keycode::LControl, Keycode::LeftBracket};
+use std::{thread, time::Duration};
+use windows::Win32::UI::Input::Ime::ImmDisableIME;
 
 fn main() {
-    let mut hk = hotkey::Listener::new();
-    hk.register_hotkey(
-        hotkey::modifiers::CONTROL,
-        KeyboardAndMouse::VK_OEM_4.0 as u32,
-        || {
-            println!("Ctrl-[ pressed.");
-            imdisable().expect("WARN: couldn't disable ime.");
-        },
-    )
-    .unwrap();
-
-    hk.listen();
-}
-
-fn imdisable() -> windows::core::Result<()> {
-    unsafe {
-        let fw = GetForegroundWindow();
-        let ime = ImmGetDefaultIMEWnd(fw);
-
-        let res = SendMessageA(
-            ime,
-            WM_IME_CONTROL,
-            WPARAM(IMC_SETOPENSTATUS as usize),
-            LPARAM(0),
-        );
-
-        println!("{:?}", res);
+    let device_state = DeviceState::new();
+    loop {
+        let keys = device_state.get_keys();
+        if keys.contains(&LControl) && keys.contains(&LeftBracket) {
+            unsafe { ImmDisableIME(0u32) };
+            thread::sleep(Duration::from_millis(500));
+        }
     }
-    Ok(())
 }
