51a5fd7ebc00637a8e91548e21c653cd197a1307
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2023-07-27 20:36:07 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 use device_query::{DeviceQuery, DeviceState, Keycode::LControl, Keycode::LeftBracket};
 use std::{thread, time::Duration};
-use windows::Win32::UI::Input::Ime::ImmDisableIME;
+use windows::Win32::{
+    Foundation::{LPARAM, WPARAM},
+    UI::{
+        Input::Ime::{ImmDisableIME, ImmGetDefaultIMEWnd, IMC_SETOPENSTATUS},
+        WindowsAndMessaging::{GetForegroundWindow, SendMessageA, WM_IME_CONTROL},
+    },
+};
 
 fn main() {
     let device_state = DeviceState::new();
     loop {
         let keys = device_state.get_keys();
         if keys.contains(&LControl) && keys.contains(&LeftBracket) {
-            unsafe { ImmDisableIME(0u32) };
+            imdisable().ok();
             thread::sleep(Duration::from_millis(500));
         }
     }
+}
+
+fn imdisable() -> anyhow::Result<()> {
+    unsafe {
+        let fw = GetForegroundWindow();
+        let ime = ImmGetDefaultIMEWnd(fw);
+        let res = SendMessageA(
+            ime,
+            WM_IME_CONTROL,
+            WPARAM(IMC_SETOPENSTATUS as usize),
+            LPARAM(0),
+        );
+        dbg!(&res);
+    }
+    Ok(())
 }
